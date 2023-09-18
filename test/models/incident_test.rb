34b00d08838fdcd25a4a6d1ba4b4a4ade0c49e6d
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-09-18 20:32:20 +0700

    Create a unit test for incident model

--- test/models/incident_test.rb
+++ test/models/incident_test.rb
@@ -4,4 +4,24 @@ class IncidentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "is valid with valid attributes" do
+    incident = Incident.new(title: "testing")
+    assert incident.save
+  end
+
+  test "is not valid without a title" do
+    incident = Incident.new
+    assert_not incident.save
+  end
+
+  test "is valid with valid severity value" do
+    incident = Incident.new(title: "testing", severity: "sev0")
+    assert incident.save
+  end
+
+  test "should raise ArgumentError when severity value is not valid" do
+    assert_raises(ArgumentError) do
+      Incident.new(title: "testing", severity: "sev5")
+    end
+  end
 end
