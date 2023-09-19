2abfc42ee315865ef84200759490c5c4aff4f127
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-09-19 15:32:54 +0700

    Connect to the slack and then check the token received from a request by slack

--- app/controllers/rootlies_controller.rb
+++ app/controllers/rootlies_controller.rb
@@ -3,6 +3,19 @@ class RootliesController < ApplicationController
 
   # POST /rootly
   def rootly
-    render json: { text: 'success' }
+    payload = {
+      text: 'success'
+    }
+    check_token_slack!
+    render json: payload, status: 200
+  rescue => e
+    payload[:text] = e.message
+    render json: payload, status: 200
+  end
+
+  private
+
+  def check_token_slack!
+    raise 'the application is not authorized!' unless request.params['token'] == ENV['SLACK_TOKEN']
   end
 end
