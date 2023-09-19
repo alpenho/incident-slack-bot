ce21bc5fc30ae10b4382b57310488a22f7043e89
Muhammad Alpen Saputra Halim <alpensaputra@outlook.com>
2023-09-19 15:33:37 +0700

    Adding a regex to get a parameter when doing a declare features

--- app/controllers/rootlies_controller.rb
+++ app/controllers/rootlies_controller.rb
@@ -1,12 +1,16 @@
 class RootliesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  PARAMETER_INCIDENT_REGEX = /<(.*?)>/
+
   # POST /rootly
   def rootly
     payload = {
       text: 'success'
     }
     check_token_slack!
+    incident_parameters = get_params
+    payload[:text] = "title #{incident_parameters[0]}, description #{incident_parameters[1]}, severity #{incident_parameters[2]}"
     render json: payload, status: 200
   rescue => e
     payload[:text] = e.message
@@ -17,5 +21,9 @@ class RootliesController < ApplicationController
 
   def check_token_slack!
     raise 'the application is not authorized!' unless request.params['token'] == ENV['SLACK_TOKEN']
+  end
+
+  def get_params
+    request.params['text'].scan(PARAMETER_INCIDENT_REGEX).flatten
   end
 end
